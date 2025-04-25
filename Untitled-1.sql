0ecbcd9e85e0d1a77d7c7ce0c45504efde9190e6
VitalosTunde <12tundi@gmail.com>
2025-04-26 01:06:43 +0200

    Add files via upload

---
+++ Untitled-1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Hazifeladat(
+	LOGIN nvarchar(255) MASKED WITH (Function = 'partial(1,"XXX",1)') NOT NULL ,
+	EMAIL nvarchar(255) MASKED WITH (function = 'email()') NOT NULL,
+	NEV nvarchar(255) NOT NULL,
+	SZULEV int MASKED WITH (FUNCTION = 'random(1,100)') NULL,
+	NEM nvarchar(1) NULL,
+	CIM nvarchar(255) NULL,
+PRIMARY KEY (LOGIN))
+
+INSERT INTO Hazifeladat (LOGIN, EMAIL, NEV, SZULEV, NEM, CIM)
+SELECT LOGIN, EMAIL, NEV, SZULEV, NEM, CIM FROM Ugyfel;
+
+CREATE USER MaskUser WITHOUT LOGIN;
+
+GRANT SELECT ON Hazifeladat TO MaskUser;
+
+-- GRANT UNMASK TO MaskUser;
+
+EXECUTE AS USER = 'MaskUser';
+SELECT * FROM Hazifeladat;
+REVERT;
